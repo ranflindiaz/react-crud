212e06ddcecd474b00f0d496680be9a2707f500f
ranflindiaz <54969258+ranflindiaz@users.noreply.github.com>
2021-02-12 21:53:36 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Tareas</h1>
+      <hr/>
+       
+    </div>
+  );
+}
+
+export default App;
